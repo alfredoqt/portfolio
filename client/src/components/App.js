a5d65002ef21152355d484af656417a41cd855da
AlfredoQT <alfredoq98@hotmail.com>
2018-11-26 23:05:31 -0800

    Prototyping the sidebar

---
+++ client/src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Avatar from './Avatar';
+import Sidebar from './Sidebar';
+
+class App extends Component {
+  render() {
+    return (
+      <Sidebar />
+    );
+  }
+}
+
+export default App;
---
+++ client/src/components/Sidebar.js
@@ -0,0 +1,118 @@
+import React from 'react';
+import Avatar from './Avatar';
+
+import Icon from '@mdi/react'
+import { mdiAccount, mdiFileDocumentOutline, mdiEmail, mdiNewspaper, mdiPoll } from '@mdi/js'
+
+function Sidebar() {
+  return(
+    <aside style={{
+      backgroundColor: '#283e4a',
+      width: '20%',
+      height: '100vh',
+      display: 'flex',
+      alignItems: 'center',
+      flexDirection: 'column',
+    }}>
+      <div style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        padding: '20px 0',
+      }}>
+        <Avatar style={{
+          marginBottom: '15px',
+        }}/>
+        <p style={{
+          color: '#c7d1d8',
+          marginBottom: '10px',
+          textTransform: 'uppercase',
+          fontSize: '20px',
+          fontWeight: 'bold',
+        }}>Alfredo Quintero</p>
+        <p style={{
+          color: '#c7d1d8',
+          textTransform: 'uppercase',
+        }}>Full Stack Developer</p>
+      </div>
+      <div style={{
+        display: 'flex',
+        flexDirection: 'column',
+        width: '100%',
+      }}>
+        <div style={{
+          padding: '8px 0',
+          color: '#c7d1d8',
+          display: 'flex',
+          alignItems: 'center'
+        }}>
+          <Icon path={mdiAccount}
+            size={1.2}
+            color="#c7d1d8"
+            style={{
+              margin: '0 15px',
+            }}/>
+          <span>About</span>
+        </div>
+        <div style={{
+          padding: '8px 0',
+          color: '#c7d1d8',
+          display: 'flex',
+          alignItems: 'center'
+        }}>
+          <Icon path={mdiNewspaper}
+            size={1.2}
+            color="#c7d1d8"
+            style={{
+              margin: '0 15px',
+            }}/>
+          <span>Projects</span>
+        </div>
+        <div style={{
+          padding: '8px 0',
+          color: '#c7d1d8',
+          display: 'flex',
+          alignItems: 'center'
+        }}>
+          <Icon path={mdiPoll}
+            size={1.2}
+            color="#c7d1d8"
+            style={{
+              margin: '0 15px',
+            }}/>
+          <span>Skills</span>
+        </div>
+        <div style={{
+          padding: '8px 0',
+          color: '#c7d1d8',
+          display: 'flex',
+          alignItems: 'center'
+        }}>
+          <Icon path={mdiFileDocumentOutline}
+            size={1.2}
+            color="#c7d1d8"
+            style={{
+              margin: '0 15px',
+            }}/>
+          <span>Resume</span>
+        </div>
+        <div style={{
+          padding: '8px 0',
+          color: '#c7d1d8',
+          display: 'flex',
+          alignItems: 'center'
+        }}>
+          <Icon path={mdiEmail}
+            size={1.2}
+            color="#c7d1d8"
+            style={{
+              margin: '0 15px',
+            }}/>
+          <span>Contact</span>
+        </div>
+      </div>
+    </aside>
+  );
+}
+
+export default Sidebar;
